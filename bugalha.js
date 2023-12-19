1f7a7406d8cef7e9ea4cc1b4fc41bb3a502b7858
BrunoCesarioMenezes <bruno.cesario@estudante.ufjf.br>
2023-12-18 22:33:10 -0300

    randomiza dado e jogada do bot

--- bugalha.js
+++ bugalha.js
@@ -1,3 +1,4 @@
+
 let ptJog = [0, 0, 0];
 let ptBot = [0, 0, 0];
 
@@ -7,3 +8,12 @@ let somaJog = 0;
 let somaBot = 0;
 let dado;
 let vez = 1;
+
+function girarDado() {
+    return dado = Math.floor(Math.random() * 6) + 1;
+}
+
+function limitaColuna() {
+    let indice = Math.floor(Math.random() * 3);
+    return indice;
+}
