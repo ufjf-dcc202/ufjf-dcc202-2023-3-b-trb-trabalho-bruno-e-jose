47832ade45a2d0aacaf7df6d71dbba5a73cc3d2c
BrunoCesarioMenezes <bruno.cesario@estudante.ufjf.br>
2023-12-18 22:30:51 -0300

    cria bugalha.js e declara variaveis

---
+++ bugalha.js
@@ -0,0 +1,9 @@
+let ptJog = [0, 0, 0];
+let ptBot = [0, 0, 0];
+
+let tabJogador = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+let tabBot = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+let somaJog = 0;
+let somaBot = 0;
+let dado;
+let vez = 1;
